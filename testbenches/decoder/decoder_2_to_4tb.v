bc21ca5ae5f2bbfd9f7637f676320497d66744d6
André Nakkab <79466926+ajn313@users.noreply.github.com>
2026-02-14 03:47:55 -0500

    Update decoder_2_to_4tb.v

--- testbenches/decoder/decoder_2_to_4tb.v
+++ testbenches/decoder/decoder_2_to_4tb.v
@@ -14,17 +14,17 @@ module tb_decoder_2_to_4;
     initial begin
         // Test 0
         in = 2'b00; #10;
-        if (out == 4'b0001) $display("Test 0 passed!");
+        if (out == 4'b0001) $display("Test 0 passed");
         else $display("Test 0 failed!");
 
         // Test 1
         in = 2'b01; #10;
-        if (out == 4'b0010) $display("Test 1 passed!");
+        if (out == 4'b0010) $display("Test 1 passed");
         else $display("Test 1 failed!");
 
         // Test 2
         in = 2'b10; #10;
-        if (out == 4'b0100) $display("Test 2 passed!");
+        if (out == 4'b0100) $display("Test 2 passed");
         else $display("Test 2 failed!");
 
         // Test 3
